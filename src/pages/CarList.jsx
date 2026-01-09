1007c9b4e69729a8ba0856741a7d848687837109
emre <emre@unknown52c8dc1ee60f.home>
2026-01-09 23:30:27 +0300

    fix: onLoad callback'inde event parametresi eklendi

--- src/pages/CarList.jsx
+++ src/pages/CarList.jsx
@@ -352,7 +352,7 @@ function CarList() {
                         e.target.src = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjMwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cmVjdCB3aWR0aD0iNDAwIiBoZWlnaHQ9IjMwMCIgZmlsbD0iI2Y1ZjVmNSIvPjx0ZXh0IHg9IjUwJSIgeT0iNTAlIiBmb250LWZhbWlseT0iQXJpYWwiIGZvbnQtc2l6ZT0iMjAiIGZpbGw9IiM5OTkiIHRleHQtYW5jaG9yPSJtaWRkbGUiIGR5PSIuM2VtIj5BcmHDpyBSZXNtaTwvdGV4dD48L3N2Zz4=';
                       }
                     }}
-                    onLoad={() => {
+                    onLoad={(e) => {
                       // Resim başarıyla yüklendiğinde logla
                       console.log(`✅ CarList [${index}]: Resim yüklendi - ${car.brand || car.Brand} ${car.model || car.Car_Name}`);
                       console.log(`  🔗 URL:`, e.target.src);
